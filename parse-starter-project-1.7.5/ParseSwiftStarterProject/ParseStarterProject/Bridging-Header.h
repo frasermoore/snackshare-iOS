a0b2627190d800789617b230eb913282f97c324c
Fraser Moore <fmoore@ifwe.co>
2015-07-20 20:58:53 -0700

    Added Facebook login

---
+++ parse-starter-project-1.7.5/ParseSwiftStarterProject/ParseStarterProject/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  FacebookApiBridge.h
+//  SnackShare
+//
+//  Created by Fraser Moore on 7/20/15.
+//  Copyright (c) 2015 Parse. All rights reserved.
+//
+
+#ifndef SnackShare_Bridging_Header_h
+#define SnackShare_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
--- parse-starter-project-1.7.5/ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
+++ parse-starter-project-1.7.5/ParseSwiftStarterProject/ParseStarterProject/ViewController.swift
@@ -7,21 +7,88 @@
 import UIKit
 import Parse
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         let testObject = PFObject(className: "TestObject")
         testObject["foo"] = "bar"
-        testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-            println("Object has been saved.")
+       // testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
+       //     println("Object has been saved.")
+       // }
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        if (FBSDKAccessToken.currentAccessToken() != nil)
+        {
+            // User is already logged in, do work such as go to next view controller.
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            returnUserData()
         }
+        else
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        println("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        println("User Logged Out")
+    }
+    
+    
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if ((error) != nil)
+            {
+                // Process error
+                println("Error: \(error)")
+            }
+            else
+            {
+                println("fetched user: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                println("User Name is: \(userName)")
+            //    let userEmail : NSString = result.valueForKey("email") as! NSString
+            //    println("User Email is: \(userEmail)")
+            }
+        })
+    }
+    
 }
 
