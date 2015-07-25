ae000f3232b8f6c3c736023e6d825a1869e87f00
Ranit Dubey <rdubey@ifwe.co>
2015-07-24 21:15:40 -0700

    login view controller

---
+++ parse-starter-project-1.7.5/ParseSwiftStarterProject/ParseStarterProject/LoginViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//
+//  Copyright 2011-present Parse Inc. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewWillAppear(animated: Bool) {
+        if ((PFUser.currentUser()) != nil) {
+            self.performSegueWithIdentifier("showFeedViewController", sender: self)
+        }
+    }
+    
+    func loginUser() {
+        var user = PFUser()
+        user.username = self.usernameTextField.text!
+        user.password = self.passwordTextField.text!
+
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo?["error"] as? NSString
+                // Show the errorString somewhere and let the user try again.
+            } else {
+                // Hooray! Let them use the app now.
+                println("Parse user created.")
+                self.performSegueWithIdentifier("showFeedViewController", sender: self)
+            }
+        }
+    }
+
+    @IBAction func signUpButtonClicked(sender: UIButton) {
+        loginUser()
+    }
+}
+
